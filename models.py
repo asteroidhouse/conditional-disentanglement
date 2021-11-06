27927a2701ca9319e4589dfb974d1e7ddea92696
Paul Vicol <pvicol@cs.toronto.edu>
2021-11-05 20:17:53 -0400

    Updated tabs in models.py

--- models.py
+++ models.py
@@ -5,22 +5,22 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class MLP(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
-        super(MLP, self).__init__()
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.output_dim = output_dim
+  def __init__(self, input_dim, hidden_dim, output_dim):
+    super(MLP, self).__init__()
+    self.input_dim = input_dim
+    self.hidden_dim = hidden_dim
+    self.output_dim = output_dim
 
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
-        self.fc3 = nn.Linear(hidden_dim, output_dim)
+    self.fc1 = nn.Linear(input_dim, hidden_dim)
+    self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+    self.fc3 = nn.Linear(hidden_dim, output_dim)
 
-    def forward(self, x):
-        out = x.reshape(x.shape[0], -1)  # Flatten x
-        out = F.relu(self.fc1(out))
-        out = F.relu(self.fc2(out))
-        out = self.fc3(out)
-        return out
+  def forward(self, x):
+    out = x.reshape(x.shape[0], -1)
+    out = F.relu(self.fc1(out))
+    out = F.relu(self.fc2(out))
+    out = self.fc3(out)
+    return out
 
 
 class SimpleConvNet(nn.Module):
@@ -57,8 +57,8 @@ class SimpleConvNet(nn.Module):
     self.fc = nn.Linear(2*2048, output_dim)
 
   def forward(self, x):
-        out = self.layer1(x)
-        out = self.layer2(out)
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
-        return out
+    out = self.layer1(x)
+    out = self.layer2(out)
+    out = out.view(out.size(0), -1)
+    out = self.fc(out)
+    return out
