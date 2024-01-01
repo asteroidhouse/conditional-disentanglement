b8e64c83f066cdda80d912c891667d9763d4e0d6
Paul Vicol <pvicol@cs.toronto.edu>
2023-12-31 22:44:27 -0500

    Cleanup load_and_eval_corr.py

--- load_and_eval_corr.py
+++ load_and_eval_corr.py
@@ -1,10 +1,8 @@
-# TODO: Figure out how to visualize the saliency maps for different trained models
-
-# 1. Load a model that has been trained from a directory like saves_multi/multi_mnist_cls/...
-# 2. Load the original data that it was trained on (re-create the dataset), and make sure that we
-#    can run a forward pass of the model and it works
-# 3. Figure out how to incorporate the saliency map on top of the image
-# 4. Figure out how to evaluate the models on different test-time correlations in a range from [-1,1]
+"""
+Example
+-------
+python load_and_eval_corr.py
+"""
 import os
 import sys
 import pdb
@@ -12,6 +10,7 @@ import itertools
 import numpy as np
 import pickle as pkl
 from collections import defaultdict
+
 from ruamel.yaml import YAML
 yaml = YAML()
 yaml.preserve_quotes = True
@@ -90,8 +89,10 @@ def evaluate(dataloader):
   f1_acc = num_correct_left / float(num_total)
   f2_acc = num_correct_right / float(num_total)
 
-  f1_acc_dict = {combo: num_correct_f1_dict[combo] / float(num_total_dict[combo]) for combo in num_total_dict.keys()}
-  f2_acc_dict = {combo: num_correct_f2_dict[combo] / float(num_total_dict[combo]) for combo in num_total_dict.keys()}
+  f1_acc_dict = {combo: num_correct_f1_dict[combo] / float(num_total_dict[combo])
+                 for combo in num_total_dict.keys()}
+  f2_acc_dict = {combo: num_correct_f2_dict[combo] / float(num_total_dict[combo])
+                 for combo in num_total_dict.keys()}
 
   all_z_np = np.concatenate(all_z_np)
   all_labels = np.concatenate(all_labels)
@@ -119,16 +120,11 @@ ckpt_prefix = ''
 # ckpt_prefix = 'bva-'
 # ckpt_prefix = 'bvl-'
 
-# base_dirs = ['saves_multi/multi_mnist_cls', 'saves_multi/multi_mnist_cond', 'saves_multi/multi_mnist_uncond']
-
-
-# THE FIRST DIRECTORY HERE WAS USED JUST A SECOND AGO (OCTOBER 28?)
-# base_dirs = ['saves_multi_many/multi_mnist_cls', 'saves_multi_many/multi_mnist_cond', 'saves_multi_many/multi_mnist_uncond']
-base_dirs = ['saves_multi_many_rerun_uncond/multi_mnist_uncond']
-
-
-
-# base_dirs = ['saves_multi_combined/multi_mnist_cls', 'saves_multi_combined/multi_mnist_cond', 'saves_multi_combined/multi_mnist_uncond']
+base_dirs = [
+    'saves/multi_mnist_cls',
+    'saves/multi_mnist_cond',
+    'saves/multi_mnist_uncond'
+]
 
 for base_dir in base_dirs:
   print('Base dir: {}'.format(base_dir))
@@ -136,16 +132,19 @@ for base_dir in base_dirs:
 
   for subdir in subdirs:
     try:
-      # if os.path.exists(os.path.join(base_dir, subdir, 'test_corr_range.pkl')):
-      #   continue
+      if os.path.exists(os.path.join(base_dir, subdir, 'test_corr_range.pkl')):
+        continue
 
       args = yaml.load(open(os.path.join(base_dir, subdir, 'args.yaml'), 'r'))
 
       subspace_dim = args['z_dim'] // 2
-
       model = torch.load(os.path.join(base_dir, subdir, '{}model.pt'.format(ckpt_prefix)))
-      f1_classifier = torch.load(os.path.join(base_dir, subdir, '{}f1_classifier.pt'.format(ckpt_prefix)))
-      f2_classifier = torch.load(os.path.join(base_dir, subdir, '{}f2_classifier.pt'.format(ckpt_prefix)))
+      f1_classifier = torch.load(
+          os.path.join(base_dir, subdir, '{}f1_classifier.pt'.format(ckpt_prefix))
+      )
+      f2_classifier = torch.load(
+          os.path.join(base_dir, subdir, '{}f2_classifier.pt'.format(ckpt_prefix))
+      )
 
       model = model.to(device)
       f1_classifier = f1_classifier.to(device)
@@ -157,12 +156,14 @@ for base_dir in base_dirs:
       # Create test data with a range of correlations between -1 and 1
       for test_corr in test_corr_list:
         classes = [3, 8]
-        _, _, testset_correlated, c_to_i, i_to_c = data_utils.get_correlated_data('mnist',
-                                                                                  classes=classes,
-                                                                                  train_corr=0.0,
-                                                                                  test_corr=test_corr,
-                                                                                  noise=args['noise'],
-                                                                                  occlusion_patch_size=args['occlusion_patch_size'])
+        _, _, testset_correlated, c_to_i, i_to_c = data_utils.get_correlated_data(
+            'mnist',
+            classes=classes,
+            train_corr=0.0,
+            test_corr=test_corr,
+            noise=args['noise'],
+            occlusion_patch_size=args['occlusion_patch_size']
+        )
 
         possible_labels = [c_to_i[v] for v in classes]
         test_loader_correlated = DataLoader(testset_correlated, batch_size=100, shuffle=True)
